3ce33185a4232dc9c9954c0bcb9f0c65bbdc1f56
Darren <chismdarren@yahoo.com>
2025-03-06 12:00:17 -0600

    Deploy to GitHub Pages

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import Firebase SDK
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeCYpS1JV5gJWD8qWsnVKenwgbDrIt_h8",
+  authDomain: "makeupbyny-1.firebaseapp.com",
+  projectId: "makeupbyny-1",
+  storageBucket: "makeupbyny-1.appspot.com",
+  messagingSenderId: "327675302548",
+  appId: "1:327675302548:web:581f25c2c6aebaab629a81",
+  measurementId: "G-P8F85KTSFP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+console.log("Firebase Initialized Successfully!");
